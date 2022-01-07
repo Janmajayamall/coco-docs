279e2786f5f12c9d263f33d07f0788ac64d6d1be
Janamajaya Mall <janmajaya.choithram@gmail.com>
2022-01-07 20:58:04 +0800

    updated pending docs

---
+++ sidebar.js
@@ -0,0 +1,27 @@
+module.exports = {
+	docs: [
+		{
+			type: "category",
+			label: "Welcome",
+			items: ["welcome/what-is-coco", "welcome/overview"],
+		},
+		{
+			type: "category",
+			label: "User guides",
+			items: [
+				"user-guides/getting-started",
+				"user-guides/groups",
+				"user-guides/posts",
+			],
+		},
+		{
+			type: "category",
+			label: "More about COCO",
+			items: [
+				"more-about-coco/post-lifecycle",
+				"more-about-coco/group-governing-body",
+				"more-about-coco/groups-markets-curation",
+			],
+		},
+	],
+};
