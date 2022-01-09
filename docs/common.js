ab47cd0667812fdc684aa8af46e9a5786ef4e849
Janamajaya Mall <janmajaya.choithram@gmail.com>
2022-01-09 18:04:42 +0800

    ATTACH IMAGE tags

--- docs/common.js
+++ docs/common.js
@@ -4,10 +4,8 @@ export const Image = ({ src, caption }) => {
 	return (
 		<center width="100%">
 			<figure>
-				<img src={src} width="60%" />
-				<figcaption style={{ fontWeight: "bold" }}>
-					{caption}
-				</figcaption>
+				<img src={src} width="70%" />
+				<figcaption style={{}}>{caption}</figcaption>
 			</figure>
 		</center>
 	);
